1581c00b940d0c8b9961d0b850c67f318afebe60
Noel Namai <noelnamai@fas.harvard.edu>
2014-10-21 19:58:11 -0400

    update

--- frequent_words_problem.py
+++ frequent_words_problem.py
@@ -1,5 +1,3 @@
-## Python function that finds the most frequent k-mers in a string given a DNA string Text and an integer k.
-
 ## Find the most frequent k-mers in a string.
 ## Input: A string Text and an integer k.
 ## Output: All most frequent k-mers in Text.
@@ -13,8 +11,8 @@ def frequent_words_problem(text, k):
     max_count = max(count)
     for i in range(len(text) - k):
         if count[i] == max_count:
-            frequent_patterns.append(text[i : k+i])            
-    return set(frequent_patterns) 
+            frequent_patterns.append(text[i : k+i])  
+    return " ".join(str(x) for x in set(frequent_patterns))
 
 string = "".join(open("./data/frequent_words_problem.txt")).split()
 frequent_words_problem(string[0], int(string[1]))

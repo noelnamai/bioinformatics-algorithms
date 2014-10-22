f31ac6885392c498ee871ba382702aa6f6d9088f
Noel Namai <noelnamai@fas.harvard.edu>
2014-10-21 22:51:21 -0400

    update

--- pattern_matching_problem.py
+++ pattern_matching_problem.py
@@ -1,13 +1,14 @@
-def pattern_matching_problem(string_1, string_2):
+## Pattern Matching Problem: Find all occurrences of a pattern in a string.
+## Input: Strings Pattern and Genome.
+## Output: All starting positions in Genome where Pattern appears as a substring.
+
+def pattern_matching_problem(pattern, genome):    
+    position = []
+    k = len(pattern)        
+    for i in range(len(genome)):
+        if pattern == genome[i: i+k]:
+            position.append(str(i))            
+    return " ".join(position)
     
-    pos = []
-    k = len(string_1)
-        
-    for i in range(len(string_2)):
-        if string_1 == string_2[i: i+k]:
-            pos.append(str(i))
-            
-    return " ".join(pos)
-    
-string = open('pattern_matching_problem_text.txt').read().split()
+string = "".join(open("./data/pattern_matching_problem.txt")).split()
 pattern_matching_problem(string[0], string[1])

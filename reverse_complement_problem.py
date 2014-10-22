50f7579af17a612c1d5675306cce8f0f5e0fc390
Noel Namai <noelnamai@fas.harvard.edu>
2014-10-21 22:06:32 -0400

    update

--- reverse_complement_problem.py
+++ reverse_complement_problem.py
@@ -1,10 +1,12 @@
 ## Python function that finds the reverse complement of a DNA string given a DNA string Pattern. 
-
-def reverse_complement_problem(string):
-    
+## Reverse Complement Problem: Find the reverse complement of a DNA string.
+## Input: A DNA string Pattern.
+## Output: Pattern, the reverse complement of Pattern.
+                
+def reverse_complement_problem(string):    
     dictionary = {"A":"T", "T":"A", "C":"G", "G":"C"}
     output = [dictionary[x] for x in string[::-1]]
     return "".join(output)
     
-string = "".join(open('reverse_complement_problem_text.txt'))
-reverse_complement_problem(string)
+string = "".join(open("./data/reverse_complement_problem.txt")).split()
+reverse_complement_problem(string[0])
